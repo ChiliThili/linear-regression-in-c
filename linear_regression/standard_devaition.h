19d5d964f774824e868e9981e50fdecfc047e644
ChiliThili <78212761+ChiliThili@users.noreply.github.com>
2023-03-25 19:38:56 +0100

    Add files via upload

--- linear_regression/standard_devaition.h
+++ linear_regression/standard_devaition.h
@@ -2,6 +2,8 @@
 #define STANDARD_DEVIATION_H
 
 double calculateSD(double data[], int size);
+double covariance(double data_y[], double data_x[], int n);
+double correlation(double calculateSDY, double calculateSDX, double covariance);
 
 #endif
 
